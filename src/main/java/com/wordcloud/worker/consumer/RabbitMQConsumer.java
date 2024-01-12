f021f25d94154605eee042d090b7a6ef380a21c9
Taeri <taesaa@ttu.ee>
2024-01-12 13:18:35 +0200

    use wordcountservice in consumer

--- src/main/java/com/wordcloud/worker/consumer/RabbitMQConsumer.java
+++ src/main/java/com/wordcloud/worker/consumer/RabbitMQConsumer.java
@@ -1,6 +1,9 @@
 package com.wordcloud.worker.consumer;
 
 import com.wordcloud.worker.dto.UploadDto;
+import com.wordcloud.worker.model.Word;
+import com.wordcloud.worker.repository.WordRepository;
+import com.wordcloud.worker.service.WordCountService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -9,9 +12,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class RabbitMQConsumer {
     private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQConsumer.class);
+    private final WordCountService wordCountService;
+
+    public RabbitMQConsumer(WordCountService wordCountService) {
+        this.wordCountService = wordCountService;
+    }
 
     @RabbitListener(queues = {"${rabbitmq.queue.name}"})
     public void consume(UploadDto uploadDto) {
+        wordCountService.GetResult(uploadDto);
         LOGGER.info(String.format("Received message -> %s", uploadDto));
     }
 }
