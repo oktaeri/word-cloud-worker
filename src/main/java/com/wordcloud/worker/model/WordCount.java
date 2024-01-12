5d0eeae4648c7f87276afb60a114609be3948e08
Taeri <taesaa@ttu.ee>
2024-01-12 13:18:08 +0200

    add wordcount entity class

---
+++ src/main/java/com/wordcloud/worker/model/WordCount.java
@@ -0,0 +1,31 @@
+package com.wordcloud.worker.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@Entity
+@Table(name = "word_count")
+public class WordCount extends BaseEntity {
+
+    @NotNull
+    private Integer count;
+
+    @ManyToOne
+    @JoinColumn(name = "user_token_id", referencedColumnName = "id")
+    private UserToken userToken;
+
+    @ManyToOne
+    @JoinColumn(name = "word_id", referencedColumnName = "id")
+    private Word word;
+}
